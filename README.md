310f8e75d1431dbc7b7248cbd91d115da6153cf0
Vergil Iliev <archturion64@gmail.com>
2020-04-01 14:34:54 +0200

    add basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# KindaTerribleTimeTracker
+KindaTerribleTimeTracker
+=============
+
 A .Net code application written and tested under linux that helps employees keep track of working hours.
+
+
+It stores it's data into a sqlite database and updates it every 10 min so sudden reboots won't cause major data loss.
+
+
+ToDo:
+* Export to file function
+* UI
+
+
+To build, execute  "dotnet build KindaTerribleTimeTracker.sln"
+
+First time you start the application (or if you delete the databse file) you will see Error 12 which is a recoverable error.
+
+Example for application output of a first run:
+
+	./presenceApp 
+	Error 12: Database missing or Table is corrupted!
+	Removing old table.
+	Creating new table.
+	Date: Wed 01.04.2020 Week: 14 Start Time 8:31:22 AM, End Time 4:21:48 PM
+	Date: Thu 02.04.2020 Week: 14 Start Time 9:01:02 AM, End Time 3:50:22 PM
+	Press Enter to exit...
+
+Pressing enter will stop time tracking and will close the application.
+
---
+++ SqLiteConnector/SQLiteDataAccess.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+
+namespace SqLiteConnector
+{
+    public class SQLiteDataAccess
+    {
+        private readonly string connectionString;
+
+        public SQLiteDataAccess(string connString) => connectionString = connString;
+
+        public bool CreateTable()
+        {
+            bool retVal = false;
+            using (IDbConnection cnn = new SQLiteConnection(connectionString))
+            {
+                try
+                {
+                    Console.WriteLine("Removing old table.");
+                    cnn.Execute("DROP TABLE IF EXISTS \"presence\"");
+                    Console.WriteLine("Creating new table.");
+                    cnn.Execute("CREATE TABLE \"presence\" (\"key\" INTEGER NOT NULL UNIQUE, \"calWeek\" INTEGER, \"date\" TEXT UNIQUE, \"startTime\" TEXT, \"endTime\" TEXT, \"hoursActive\" REAL, PRIMARY KEY(\"key\"))");
+                    retVal = true;
+                } catch (Exception) 
+                {
+                    Console.WriteLine($"Error 14: Failed to create Database table.");
+                }
+            }
+            return retVal;
+        }
+
+        public List<PresenceModel> GetEntries()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(connectionString))
+            {
+                try
+                {
+                    var output = cnn.Query<PresenceModel>("SELECT * FROM presence", new DynamicParameters());
+                    return output.ToList();
+                } catch (Exception) 
+                {
+                    Console.WriteLine($"Error 11: Database missing or Table is corrupted!");
+                }
+            }
+            return new List<PresenceModel>();
+        }
+
+        public bool StoreEntry(in PresenceModel pm)
+        {
+            bool retVal = false;
+            using (IDbConnection cnn = new SQLiteConnection(connectionString))
+            {
+                try
+                {
+                    cnn.Execute("insert or replace into presence (date, startTime, endTime, calWeek)values (@date, COALESCE((select startTime from presence where date = @date), @startTime), @endTime, @calWeek)", pm);
+                    retVal = true;
+                } catch (Exception) 
+                {
+                    Console.WriteLine($"Error 12: Database missing or Table is corrupted!");
+                }
+            }
+            return retVal;
+        }
+
+        public void UpdateEntry(in PresenceModel pm)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(connectionString))
+            {
+                try
+                {
+                    cnn.Execute("update presence set endTime = @endTime where date = @date", pm);
+                } catch (Exception) 
+                {
+                    Console.WriteLine($"Error 13: Database missing or Table is corrupted!");
+                }
+            }
+        }
+    }
+}
